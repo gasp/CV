cf170b159c07a82823a1f5c52e4a312daaef34f4
roxyska <roxy.weis@gmail.com>
2015-05-05 11:15:36 +0200

    add personnal website to git (finally)

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Front &amp; Back-end developer | Fanny Weisenburger</title>
+		<meta name="description" content="Fanny Weisenburger, Front &amp; Back-end developer. Works with HTML, CSS, JS, PHP, MySQL">
+		<meta name="keywords" content="HTML,CSS,JavaScript, PHP">
+		<meta name="author" content="Fanny Weisenburger">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href='http://fonts.googleapis.com/css?family=Archivo+Narrow:700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-35749357-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+	</head>
+	<body>
+		<header class="top">
+			<div class="menu_container">
+				<img src="img/logoportfolio2.png" class="logo" alt="Fanny Weisenburger" />
+				<span>
+					Front &amp; Back-end developer
+				</span>
+			</div>
+		</header>
+		<section id="container">
+			<!--right side-->
+			<section class="_l" id="right_side">
+				<section>
+					<img src="img/profil.jpg" id="profil_picture" alt="Fanny Weisenburger"/>
+					<h1>
+						Fanny Weisenburger
+					</h1>
+				</section>
+				<section class="information">
+					<div><i class="fa fa-map-marker"></i>Puteaux, France</div>
+					<div><i class="fa fa-envelope-o"></i><a href="mailto:fanny.weisenburger@supinternet.fr">fanny.weisenburger@supinternet.fr</a></div>
+					<div><i class="fa fa-clock-o"></i>Joined on February 23, 1992</div>
+					<div><i class="fa fa-briefcase"></i>Available for hire from July</div>
+				</section>
+				<section id="organizations">
+					<h2>Organizations</h2>
+					<a href="https://www.linkedin.com/pub/fanny-weisenburger/4a/561/9a4" target="_blank" class="icone_btn _1" title="linkedin"><img src="img/linkedin.png" class="_l" alt="linkedin" /></a>
+					<a href="https://github.com/roxyska" target="_blank" class="icone_btn" title="github"><img src="img/github.png" class="_l" alt="github" /></a>
+					<a href="http://stackoverflow.com/users/1988476/roxyska" target="_blank" class="icone_btn" title="stackoverflow"><img src="img/stackoverflow.png" class="_l" alt="stackoverflow" /></a>
+				</section>
+			</section>
+			<!--content-->
+			<section class="_l" id="content">
+				<div class="bloc_container">
+					<section class="_l _small bloc">
+						<h2>Popular repositories</h2>
+						<ul>
+							<li><a href="http://leaguesaffinity.com/" target="_blank" class="icone_btn" title="league's affinity"><i class="fa fa-book"></i><span class="repo">League's Affinity</span><div class="description">end-of-study project</div></a></li>
+							<li><a href="http://jsfiddle.net/KvVSd/65/" target="_blank" class="icone_btn" title="circle's animation"><i class="fa fa-book"></i><span class="repo">Circle's animation</span><div class="description">JavaScript animation</div></a></li>
+							<li><a href="https://github.com/roxyska/FileManager/" target="_blank" class="icone_btn" title="Filemanager"><i class="fa fa-book"></i><span class="repo">Filemanager</span><div class="description">Filemanager in PHP</div></a></li>
+						</ul>
+					</section>
+					<section class="_r _small bloc">
+						<h2>Repositories contributed to</h2>
+						<ul>
+							<li><a href="http://www.meltygroup.com/" target="_blank" class="icone_btn" title="MeltyGroup"><i class="fa fa-group"></i><span class="repo">MeltyGroup</span><div class="description">3rd year internship</div></a></li>
+							<li><a href="http://www.soonvibes.com/" target="_blank" class="icone_btn" title="Soonvibes"><i class="fa fa-group"></i><span class="repo">Soonvibes</span><div class="description">2nd year internship</div></a></li>
+							<li><a href="http://www.ruedelapaix.com/" target="_blank" class="icone_btn" title="Yakota"><i class="fa fa-group"></i><span class="repo">Yakota</span><div class="description">1st year internship</div></a></li>
+							<li><a href="http://www.supinternet.fr/" target="_blank" class="icone_btn" title="Sup'Internet"><i class="fa fa-group"></i><span class="repo">Sup'Internet</span><div class="description">Graduate school of Web technologies</div></a></li>
+						</ul>
+					</section>
+				</div>
+				<section class="bloc desktop">
+					<h2>Your Contributions</h2>
+					<div class="timeline_container">
+						<div class="timeline">
+							<div class="top _1">
+								<div class="timeline_link"></div>
+								<div class="commit show_tooltip" data-title="Yakota"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="E-mailing"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="HTML"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="Zend"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="PHP"></div>
+							</div>
+							<div class="top _2">
+								<div class="timeline_link"></div>
+								<div class="commit show_tooltip" data-title="MeltyGroup"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="Shape"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="HTML"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="CSS"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="JS"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="PHP"></div>
+							</div>
+						</div>
+						
+						<div class="timeline middle">
+							<div class="commit" data-title="University"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="Sup'Internet"></div>
+							<div class="link"></div>
+							<div class="commit show_tooltip" data-title="HTML"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="CSS"></div>
+							<div class="link"></div>
+							<div class="commit show_tooltip" data-title="PHP"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="MySQL"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="Webdesign"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="Photoshop"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="2nd Year"></div>
+							<div class="link"></div>
+							<div class="commit show_tooltip" data-title="POO"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="Jquery"></div>
+							<div class="link"></div>
+							<div class="commit show_tooltip" data-title="JS"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="Objective C"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="C#/.net"></div>
+							<div class="link"></div>
+							<div class="commit" data-title="3rd Year"></div>
+							<div class="link"></div>
+							<div class="commit show_tooltip" data-title="end-of-study project"></div>
+						</div>
+						<div class="timeline">
+							<div class="bottom _1">
+								<div class="timeline_link"></div>
+								<div class="commit show_tooltip" data-title="Soonvibes"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="Zend"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="HTML"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="CSS"></div>
+								<div class="link"></div>
+								<div class="commit" data-title="JS"></div>
+							</div>
+						</div>
+						<div class="tips">
+							<i class="fa fa-question-circle"></i><span>for know more, hover circles</span>
+						</div>
+					</div>
+				</section>
+
+				<section class="activity">
+					<h1>Contribution Activity</h1>
+					<a href="files/CV-FannyWeisenburger.pdf" target="_blank" title="download CV in PDF format" class="icone_btn btn"><i class="fa fa-download"></i>download CV</a>
+					<section>
+						<div class="h2"><div class="inner">MeltyGroup <span> - From 01/10/2013 to 30/06/2014</span></div></div>
+						<div>
+							<div>Meltygroup is the first online media group for young people. (more than 15 millions readers by month)</div>
+							<div>
+								Front and Back-end developer, Maintenance of the websites of the group. Administration and statistics platform development for editorial teams on home-made framework called "Shape". Use of HTML, CSS, JS/JQuery, PHP, MySQL, Responsive WebDesign.
+							</div>
+						</div>
+
+						<div class="h2"><div class="inner">Soonvibes <span> - From 15/02/2013 to 15/05/2013</span></div></div>
+						<div>
+							<div>Soonvibes is a young Startup specialised on electronic music. It permits DJs to post their tracks on the website and to receive some feedback on it.</div>
+							<div>
+								Front and Back-end developer. V1 of the website <a href="soonvibes.com" title="soonvibes">soonvibes.com</a> and generic ajax pagination module development. Technologies: HTML, CSS, JS, PHP, MySQL, SVN, Zend Framework.
+							</div>
+						</div>
+
+						<div class="h2"><div class="inner">Yakota <span> - From 01/06/2012 to 30/09/2012</span></div></div>
+						<div>
+							<div>Yakota is a company specialised in online jewellery selling (diamond).</div>
+							<div>
+								Web developer, Maintenance of the websites of the group. Integration of designs made in photoshop. Newsletter development. Use of HTML, CSS, JS, Zend Framework.
+							</div>
+						</div>
+					</section>
+				</section>
+			</section>
+		</section>
+	</body>
+</html>
