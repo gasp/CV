de4254258305017c78b3cca450fd8e7ef48475cb
Roxyska <roxy.weis@gmail.com>
2015-08-03 21:30:39 +0200

    add hover functionnality and tooltip ready

--- js/script.js
+++ js/script.js
@@ -86,43 +86,45 @@ var CircleAnimation = function() {
 
 var PopularRepositories = function() {
     var repositories = {"jobs": [
-        [
-            "Sup'internet",
-            "HTML / CSS",
-            "PHP / MySQL",
-            "Webdesign",
-            "JS / jQuery",
-            "C# / .Net"
+    [
+    "sup-internet",
+    "html-css",
+    "php-mysql",
+    "webdesign",
+    "js-jquery",
+    "cSharp-dotNet"
 
-        ],
-        [
-            "Soonvibes",
-            "Zend",
-            "HTML / CSS",
-            "JS"
-        ],
-        [
-            "Melty",
-            "JS / jQuery",
-            "HTML / CSS",
-            "PHP / MySQL"
-        ],
-        [
-            "Eurelis",
-            "JS/jQuery",
-            "AngularJS",
-            "HTML / CSS",
-            "Foundation",
-            "Drupal",
-            "Magento"
-        ]
+    ],
+    [
+    "soonvibes",
+    "zend",
+    "html-css-soonvibes",
+    "js-jquery-soonvibes"
+    ],
+    [
+    "melty",
+    "js-jquery-melty",
+    "html-css-melty",
+    "php-mysql-melty"
+    ],
+    [
+    "Eurelis",
+    "js-jquery-eurelis",
+    "angularJS",
+    "html-css-eurelis",
+    "foundation",
+    "drupal",
+    "magento"
+    ]
     ]};
+
+
 
     var spaceBetweenPoints;
     var nextTopPoint;
     var nextBottomPoint;
 
-    // nextBottomPoint = lastTopPoint - ((nextTopPoint - lastTopPoint) / 2)
+    var dots = [];
 
     var topPos = 30;
     var middlePos = 70;
@@ -140,37 +142,58 @@ var PopularRepositories = function() {
         canvas_context.stroke();
     }
 
-    function drawBullet(X, Y) {
+    function drawBullet(X, Y, radius) {
+        if (typeof radius === "undefined") {
+            radius = 5;
+        }
+
         canvas_context.beginPath();
-        canvas_context.arc(X,Y,5,0,2*Math.PI);
-        canvas_context.stroke();
+        
+        canvas_context.arc(X,Y,radius,0,2*Math.PI);
+        
+        canvas_context.fill();
+        // canvas_context.strokeStyle = '#3498DB';
+        // canvas_context.stroke();
     }
 
-    function drawLineAndBullet(startX, startY, endY) {
+    function drawLineAndBullet(startX, startY, endY, text, dotNumber) {
         //create line ...
         drawLine(startX, startY, endY);
 
         //... and his own bullet
-        drawBullet(startX + spaceBetweenPoints,endY);
+        var radius = 5;
+        if (typeof text !== "undefined" && typeof dots[dotNumber] === "undefined") {
+            dots.push({
+                x: startX + spaceBetweenPoints,
+                y: endY,
+                r: 5,
+                rXr: 25,
+                tip: text,
+                isHovered: false
+            });
+
+
+        } else if (typeof dots[dotNumber] !== "undefined") {
+            // console.log("exist");
+            radius = dots[dotNumber].r;
+        }
+
+        drawBullet(startX + spaceBetweenPoints,endY, radius);
 
         //update positions
         $xPos += spaceBetweenPoints;
         $yPos = endY;
+        $dotNumber ++;
     }
 
-
-    var canvas = document.getElementById("popularRepositories");
-    if (canvas.getContext) {
-        var canvas_context = canvas.getContext("2d");
-        
-        //define text style
-        canvas_context.font = "10px sans-serif";
-        canvas_context.textAlign = "center";
-
+    function drawCanvas() {
+        // console.log("drawCanvas");
+        $dotNumber = 0;
         //initialize with first bullet
-        canvas_context.beginPath();
-        canvas_context.arc($xPos,$yPos,5,0,2*Math.PI);
-        canvas_context.stroke();
+        // canvas_context.beginPath();
+        drawBullet($xPos,$yPos)
+        // canvas_context.arc($xPos,$yPos,5,0,2*Math.PI);
+        // canvas_context.stroke();
         //create first line
         drawLineAndBullet($xPos, $yPos, middlePos);
 
@@ -178,7 +201,6 @@ var PopularRepositories = function() {
         var lastTopPos = 0;
         var lastBottomPos = 0;
         for (var i = 0; i < repositories.jobs.length; i++) {
-            console.log(repositories.jobs[i]);
             $yPos = middlePos;
             if (beginPos !== 0) {
                 $xPos = beginPos + ($xPos - beginPos) / 2;
@@ -195,23 +217,16 @@ var PopularRepositories = function() {
                     drawBullet($xPos, middlePos); 
                 }
                 beginPos = $xPos;
-                console.log(repositories.jobs[i].length);
                 for (var job = 0; job < repositories.jobs[i].length; job++) {
-                    drawLineAndBullet($xPos, $yPos, topPos);
-                    console.log(repositories.jobs[i][job]);
-                    if ((job % 2) != 1) {
-                        canvas_context.fillText(repositories.jobs[i][job], $xPos, topPos - 15);
-                    }
-                    else {
-                        canvas_context.fillText(repositories.jobs[i][job], $xPos, topPos + 17);
-                    }
+                    drawLineAndBullet($xPos, $yPos, topPos, repositories.jobs[i][job], $dotNumber);
+                    // if ((job % 2) != 1) {
+                    //     canvas_context.fillText(repositories.jobs[i][job], $xPos, topPos - 15);
+                    // }
+                    // else {
+                    //     canvas_context.fillText(repositories.jobs[i][job], $xPos, topPos + 17);
+                    // }
                     
-                }
-
-                // drawLineAndBullet($xPos, $yPos, topPos);
-                // drawLineAndBullet($xPos, $yPos, topPos);
-                // drawLineAndBullet($xPos, $yPos, topPos);
-                // drawLineAndBullet($xPos, $yPos, topPos);                
+                }              
                 lastTopPos = $xPos;
             } 
             else {
@@ -227,22 +242,94 @@ var PopularRepositories = function() {
                 beginPos = $xPos;
 
                 for (var job = 0; job < repositories.jobs[i].length; job++) {
-                    drawLineAndBullet($xPos, $yPos, bottomPos);
-                    console.log(repositories.jobs[i][job]);
-                    if ((job % 2) != 1) {
-                        canvas_context.fillText(repositories.jobs[i][job], $xPos, bottomPos + 17);
-                    }
-                    else {
-                        canvas_context.fillText(repositories.jobs[i][job], $xPos, bottomPos - 15);
-                    }
+                    drawLineAndBullet($xPos, $yPos, bottomPos, repositories.jobs[i][job], $dotNumber);
+                    // if ((job % 2) != 1) {
+                    //     canvas_context.fillText(repositories.jobs[i][job], $xPos, bottomPos + 17);
+                    // }
+                    // else {
+                    //     canvas_context.fillText(repositories.jobs[i][job], $xPos, bottomPos - 15);
+                    // }
                     
                 }
-                // drawLineAndBullet($xPos, $yPos, bottomPos);
-                // drawLineAndBullet($xPos, $yPos, bottomPos);
-                // drawLineAndBullet($xPos, $yPos, bottomPos);
                 lastBottomPos = $xPos;
             }
         }
+        canvas_context.closePath();
+    }
+
+    var canvas = document.getElementById("popularRepositories");
+    if (canvas.getContext) {
+        var canvas_context = canvas.getContext("2d");
+        var canvasOffset = $("#popularRepositories").offset();
+        var offsetX = canvasOffset.left;
+        var offsetY = canvasOffset.top;
+        
+        
+        //define text style
+        canvas_context.font = "10px sans-serif";
+        canvas_context.textAlign = "center";
+
+        canvas_context.fillStyle = '#3498DB';
+        canvas_context.strokeStyle = '#3498DB';
+        
+        drawCanvas();
+
+        // request mousemove events
+        $("#popularRepositories").mousemove(handleMouseMove);
+
+        // show tooltip when mouse hovers over dot
+        function handleMouseMove(e){
+            mouseX=parseInt(e.clientX-offsetX);
+            mouseY=parseInt(e.clientY-offsetY);
+
+            // Put your mousemove stuff here
+            var hit = false;
+            // coucou();
+            for (var i = 0; i < dots.length; i++) {
+                var dot = dots[i];
+                var dx = mouseX - dot.x;
+                var dy = mouseY - dot.y;
+                if (dx * dx + dy * dy < dot.rXr) {
+                    console.log(dot);
+                    //@todo --> tooltips
+                    canvas_context.beginPath();
+                    canvas_context.clearRect(dot.x - dot.r, dot.y - dot.r, dot.r * 2, dot.r * 2);
+                    canvas_context.closePath();
+                    dot.r = 7;
+                    dot.rXr = 100;
+                    canvas_context.beginPath();
+                    dot.isHovered = true;
+                    drawBullet(dot.x, dot.y, dot.r);
+                    canvas_context.closePath();
+                    $('.commitList#' + dot.tip).removeClass('hide').siblings().addClass('hide');
+                    $('#' + dot.tip).css("left", (dot.x) + "px");
+                    $('#' + dot.tip).css("top", (dot.y + 15) + "px");
+                    hit = true;
+                }
+            }
+            if (!hit) { 
+                // console.log('!hit'); 
+                $('.commitList').not('.hide').addClass('hide'); 
+                for (var i = dots.length - 1; i >= 0; i--) {
+                    if(dots[i].isHovered === true) {
+                        var dot = dots[i];
+                        console.log("spotted");
+                        canvas_context.beginPath();
+                        canvas_context.clearRect(dot.x - dot.r - 1, dot.y - dot.r - 1, dot.r * 2 + 2, dot.r * 2 + 2);
+                        canvas_context.closePath();
+                        // canvas_context.clearRect(0, 0, canvas.width, canvas.height);
+                        // console.log("t'es niqué");
+                        dot.r = 5;
+                        dot.rXr = 25;
+                        canvas_context.beginPath();
+                        dot.isHovered = false;
+                        drawBullet(dot.x, dot.y, dot.r);
+                        canvas_context.closePath();
+                    }
+                };
+            }
+        }
+
     }
 };
 
